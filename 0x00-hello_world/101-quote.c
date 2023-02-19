b18519a3ce85291b8809af866c013d2ecfacdc14
Yayra Vorsah <yavprincess@gmail.com>
2023-02-19 05:03:54 -0800

    More work

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,7 @@ int main(void)
 {
 	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	ssize_t len = sizeof(str) - 1;
+
 	write(STDERR_FILENO, str, len);
 	return (1);
 }
