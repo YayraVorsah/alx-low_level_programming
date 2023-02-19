82abbc9fd2f3d59dfe88942063b79f9754ae624b
Yayra Vorsah <yavprincess@gmail.com>
2023-02-19 13:48:40 -0800

    More

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num = '0';
+	{
+		putchar(num++);
+		putchar(num++);
+		putchar(num++);
+		putchar(num++);
+		putchar(num++);
+		putchar(num++);
+		putchar(num++);
+		putchar(num++);
+		putchar(num++);
+		putchar('\n');
+	}
+	return (0);
+}
+
