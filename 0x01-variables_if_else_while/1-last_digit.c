36f3eab2948b6b14333274cc1d2084bce3405500
Yayra Vorsah <yavprincess@gmail.com>
2023-02-19 09:27:13 -0800

    greater or less

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	int LastDigit = n % 10;
+
+	printf("Last digit of %d is %d ", n, lastDigit);
+
+	if (LastDigit > 5)
+	{
+		printf("and is greater than 5\n");
+	}
+	else if (LastDigit == 0)
+	{
+		printf("and is 0\n");
+	}
+	else
+	{
+		printf("and is leass than 6 and not 0\n");
+	}
+
+	return (0);
+}
