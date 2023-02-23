3033caec90de42bc160c3cbc52dbe5c19fff7334
Yayra Vorsah <yavprincess@gmail.com>
2023-02-23 13:26:37 -0800

    Nested loops

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+/**
+ * print_numbers - prints the numbers from 0 to 9 followed by a new line
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+		_putchar(num + '0');
+		num++;
+	}
+	_putchar('\n');
+}
+
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+/**
+ * more_numbers - prints the numbers from 0 to 14 ten times
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int num, count;
+
+	for (count = 0; count < 10; count++)
+	{
+		for (num = 0; num <= 14; num++)
+		{
+			if (num > 9)
+				_putchar(num / 10 + '0');
+			_putchar(num % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
+
