4485d868f82fd9ce9a1ff1b8c8166ae430b6dc1a
Yayra Vorsah <yavprincess@gmail.com>
2023-02-23 15:40:20 -0800

    Project

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	/* int i; */
+    /* i = 0; */
+    /* while (i < 10) */
+    /* { */
+    /*     putchar(i); */
+    /* } */
+	putchar('\n');
+	return (0);
+}
+
